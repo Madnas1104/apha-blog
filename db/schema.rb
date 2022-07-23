2e14195c1d91d445116a0a83c6b126297f6b333e
madhannasrin <madhanmanokar@gmail.com>
2022-07-23 19:22:09 +0530

    article creation and display

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_23_055013) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_061141) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "information"
   end
 
 end
